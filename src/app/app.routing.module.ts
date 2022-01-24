0a9963ff2efc5af63376324a4c9d9645603cbaf6
Genuine-Identity <girish.nandgawe@gmail.com>
2022-01-25 00:17:40 +0530

    1 file modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-const routes: Routes = [  
+const routes: Routes = [
   {
     path: '',
-    // loadChildren: './layout/admin/admin-layout.module#AdminLayoutModule',  
-    loadChildren: () => import('./layout/admin/admin-layout.module').then(x => x.AdminLayoutModule)  
+    // loadChildren: './layout/admin/admin-layout.module#AdminLayoutModule',
+    loadChildren: () =>
+      import('./layout/admin/admin-layout.module').then(
+        (x) => x.AdminLayoutModule
+      ),
   },
   {
     path: 'user',
     // loadChildren: './layout/default/default-layout.module#DefaultLayoutModule',
-    loadChildren: () => import('./layout/default/default-layout.module').then(x => x.AdminLayoutModule) 
+    loadChildren: () =>
+      import('./layout/default/default-layout.module').then(
+        (x) => x.DefaultLayoutModule
+      ),
   },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { onSameUrlNavigation: 'ignore', preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'ignore',
+      preloadingStrategy: PreloadAllModules,
+    }),
   ],
-  exports: [
-    RouterModule
-  ]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
