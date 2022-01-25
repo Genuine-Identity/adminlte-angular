6117eeed67a24e081ab478d36d2df0301e0769f7
Genuine-Identity <girish.nandgawe@gmail.com>
2022-01-25 15:12:32 +0530

    1 file modified

--- src/app/shared/components/content-header/content-header.component.ts
+++ src/app/shared/components/content-header/content-header.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { NavigationCancel, Event, NavigationEnd, NavigationError, NavigationStart, Router, ActivatedRoute } from '@angular/router';
+import {
+  NavigationCancel,
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  ActivatedRoute,
+} from '@angular/router';
 @Component({
   selector: 'app-content-header',
   templateUrl: './content-header.component.html',
   styleUrls: ['./content-header.component.css'],
-   encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ContentHeaderComponent implements OnInit {
   breadcrumb: any;
@@ -17,12 +25,9 @@ export class ContentHeaderComponent implements OnInit {
         while (route.firstChild) route = route.firstChild;
         return route;
       })
-      .subscribe((event:) => {
+      .subscribe((event: any) => {
         this.breadcrumb = event.data._value;
       });
   }
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
--- src/app/shared/components/content-header/content-header.module.ts
+++ src/app/shared/components/content-header/content-header.module.ts
@@ -18,3 +18,9 @@ export class ContentHeaderModule {
     }
   }
 }
+declare module "@angular/core" {
+  interface ModuleWithProviders<T = any> {
+      ngModule: Type<T>;
+      providers?: Provider[];
+  }
+}
