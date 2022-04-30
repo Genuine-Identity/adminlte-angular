37c0035b403453481a55461249dba6a6ba4b1413
Genuine-Identity <girish.nandgawe@gmail.com>
2022-04-30 23:40:50 +0530

    2 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
@@ -7,7 +7,7 @@ const routes: Routes = [
       import('./layout/admin/admin-layout.module').then(
         (x) => x.AdminLayoutModule
       ),
-  }, 
+  },
   // {
   //   path: 'user',
   //   loadChildren: () =>
