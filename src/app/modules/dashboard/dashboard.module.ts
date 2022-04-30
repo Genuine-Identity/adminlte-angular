42861527708384d114204f651949d08bc4c0e0cb
Genuine-Identity <girish.nandgawe@gmail.com>
2022-05-01 00:18:17 +0530

    1 file modified

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -9,7 +9,7 @@ import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-c
 import { DashboardRoutingModule } from './dashboard.routing.module';
 import { DashbardVersionOneComponent } from './control-panel/dashboard-version-one/dashboard-version-one.component';
 
-// import { NgChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
@@ -17,7 +17,7 @@ import { DashbardVersionOneComponent } from './control-panel/dashboard-version-o
     DashboardRoutingModule,
     FormsModule,
     CardDetailsModule,
-    // NgChartsModule,
+    NgChartsModule,
   ],
   declarations: [
     DashboardComponent,
