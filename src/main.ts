f67a375114ca0c80ea9ebccc9d81a1f9dca5e612
Genuine-Identity <girish.nandgawe@gmail.com>
2022-04-30 23:31:57 +0530

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './app/core/environment';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then((ref) => {
-    console.log('ab');
     // Ensure Angular destroys itself on hot reloads.
     if (window['ngRef']) {
       window['ngRef'].destroy();
