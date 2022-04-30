6b2590aef3b06ab8cfeece74a5059f49b7302bd0
Genuine-Identity <girish.nandgawe@gmail.com>
2022-04-30 23:58:12 +0530

    2 files modified

--- src/app/layout/admin/admin-layout.routing.module.ts
+++ src/app/layout/admin/admin-layout.routing.module.ts
@@ -17,91 +17,91 @@ const routes: Routes = [
           ),
         canActivate: [AuthGuard],
       },
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('../../modules/dashboard/dashboard.module').then(
-            (x) => x.DashboardModule
-          ),
+      // {
+      //   path: 'dashboard',
+      //   loadChildren: () =>
+      //     import('../../modules/dashboard/dashboard.module').then(
+      //       (x) => x.DashboardModule
+      //     ),
 
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: 'Dashboard',
-          breadcrumbs: 'Dashboard',
-          title: 'Dashboard Title',
-          smallText: 'Dashboard Small Text',
-          isHome: true,
-          icon: 'fa fa-home',
-          show: false,
-        },
-      },
-      {
-        path: 'admin',
-        loadChildren: () =>
-          import('../../modules/admin/admin.module').then((x) => x.AdminModule),
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: 'Admin',
-          breadcrumbs: 'Admin',
-          title: 'Admin Title',
-          smallText: 'Admin Small Text',
-          isHome: true,
-          icon: 'fa fa-home',
-          show: false,
-        },
-      },
-      {
-        path: 'mail',
-        // loadChildren: '../../modules/mail/mail.module#MailModule',
-        loadChildren: () =>
-          import('../../modules/mail/mail.module').then((x) => x.MailModule),
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: 'Mail',
-          breadcrumbs: 'Mail',
-          title: 'Dashboard Title',
-          smallText: 'Dashboard Small Text',
-          isHome: true,
-          icon: 'fa fa-home',
-          show: false,
-        },
-      },
-      {
-        path: 'search',
-        // loadChildren: '../../modules/search/search.module#SearchModule',
-        loadChildren: () =>
-          import('../../modules/search/search.module').then(
-            (x) => x.SearchModule
-          ),
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: 'Search',
-          breadcrumbs: 'Search',
-          title: 'Search Title',
-          smallText: 'Search Small Text',
-          isHome: true,
-          icon: 'fa fa-home',
-          show: false,
-        },
-      },
-      {
-        path: 'system',
-        // loadChildren: '../../modules/system/system.module#SystemModule',
-        loadChildren: () =>
-          import('../../modules/system/system.module').then(
-            (x) => x.SystemModule
-          ),
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: 'System',
-          breadcrumbs: 'System',
-          title: 'System Title',
-          smallText: 'System Small Text',
-          isHome: true,
-          icon: 'fa fa-home',
-          show: false,
-        },
-      },
+      //   canActivate: [AuthGuard],
+      //   data: {
+      //     breadcrumb: 'Dashboard',
+      //     breadcrumbs: 'Dashboard',
+      //     title: 'Dashboard Title',
+      //     smallText: 'Dashboard Small Text',
+      //     isHome: true,
+      //     icon: 'fa fa-home',
+      //     show: false,
+      //   },
+      // },
+      // {
+      //   path: 'admin',
+      //   loadChildren: () =>
+      //     import('../../modules/admin/admin.module').then((x) => x.AdminModule),
+      //   canActivate: [AuthGuard],
+      //   data: {
+      //     breadcrumb: 'Admin',
+      //     breadcrumbs: 'Admin',
+      //     title: 'Admin Title',
+      //     smallText: 'Admin Small Text',
+      //     isHome: true,
+      //     icon: 'fa fa-home',
+      //     show: false,
+      //   },
+      // },
+      // {
+      //   path: 'mail',
+      //   // loadChildren: '../../modules/mail/mail.module#MailModule',
+      //   loadChildren: () =>
+      //     import('../../modules/mail/mail.module').then((x) => x.MailModule),
+      //   canActivate: [AuthGuard],
+      //   data: {
+      //     breadcrumb: 'Mail',
+      //     breadcrumbs: 'Mail',
+      //     title: 'Dashboard Title',
+      //     smallText: 'Dashboard Small Text',
+      //     isHome: true,
+      //     icon: 'fa fa-home',
+      //     show: false,
+      //   },
+      // },
+      // {
+      //   path: 'search',
+      //   // loadChildren: '../../modules/search/search.module#SearchModule',
+      //   loadChildren: () =>
+      //     import('../../modules/search/search.module').then(
+      //       (x) => x.SearchModule
+      //     ),
+      //   canActivate: [AuthGuard],
+      //   data: {
+      //     breadcrumb: 'Search',
+      //     breadcrumbs: 'Search',
+      //     title: 'Search Title',
+      //     smallText: 'Search Small Text',
+      //     isHome: true,
+      //     icon: 'fa fa-home',
+      //     show: false,
+      //   },
+      // },
+      // {
+      //   path: 'system',
+      //   // loadChildren: '../../modules/system/system.module#SystemModule',
+      //   loadChildren: () =>
+      //     import('../../modules/system/system.module').then(
+      //       (x) => x.SystemModule
+      //     ),
+      //   canActivate: [AuthGuard],
+      //   data: {
+      //     breadcrumb: 'System',
+      //     breadcrumbs: 'System',
+      //     title: 'System Title',
+      //     smallText: 'System Small Text',
+      //     isHome: true,
+      //     icon: 'fa fa-home',
+      //     show: false,
+      //   },
+      // },
     ],
   },
 ];
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-// import "jquery";
-// import * as $ from 'jquery';
+import "jquery";
+import * as $ from 'jquery';
 import { DashboardComponent } from './dashboard.component';
 import { CardDetailsModule } from '../../shared/components/card-details/card-details.module';
 import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-control-panel.component';
