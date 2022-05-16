7145c853a939b59e0ca4bac899a80d06e7d721f4
Genuine-Identity <girish.nandgawe@gmail.com>
2022-05-16 13:12:11 +0530

    3 files modified

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms';
 import 'jquery';
 import * as $ from 'jquery';
 import { DashboardComponent } from './dashboard.component';
-import { CardDetailsModule } from '../../shared/components/card-details/card-details.module';
+// import { CardDetailsModule } from '../../shared/components/card-details/card-details.module';
 import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-control-panel.component';
 import { DashboardRoutingModule } from './dashboard.routing.module';
 import { DashbardVersionOneComponent } from './control-panel/dashboard-version-one/dashboard-version-one.component';
 
-import { NgChartsModule } from 'ng2-charts';
+// import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
     FormsModule,
-    CardDetailsModule,
-    NgChartsModule,
+    // CardDetailsModule,
+    // NgChartsModule,
   ],
   declarations: [
     DashboardComponent,
