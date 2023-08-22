e6aae3891a1bd234a9c5ce160fc8e9510bd2bd28
Genuine-Identity <girish.nandgawe@gmail.com>
2023-08-23 02:13:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-//const fs = require('fs');
-//const { join } = require('path');
+const fs = require('fs');
+const { join } = require('path');
 
-//const traverse = (dir) => {
-//    const subfolders = fs.statSync(dir).isDirectory() && fs.readdirSync(dir);
+const traverse = (dir) => {
+  const subfolders = fs.statSync(dir).isDirectory() && fs.readdirSync(dir);
 
-//    if (subfolders) {
-//        subfolders.forEach((folderName) => {
-//            const fullPath = join(dir, folderName);
+  if (subfolders) {
+    subfolders.forEach((folderName) => {
+      const fullPath = join(dir, folderName);
 
-//            if (folderName === 'node_modules') {
-//                fs.rmSync(join(dir, 'node_modules'), { recursive: true, force: true });
-//                return;
-//            }
+      if (folderName === 'node_modules') {
+        fs.rmSync(join(dir, 'node_modules'), { recursive: true, force: true });
+        return;
+      }
 
-//            traverse(fullPath);
-//        });
-//    }
-//};
+      traverse(fullPath);
+    });
+  }
+};
 
-//traverse(process.cwd());
+traverse(process.cwd());
