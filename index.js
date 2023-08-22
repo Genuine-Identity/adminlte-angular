ee2e34cb8fee9407973911576c21d030ed052ae5
Girish <girish.nandgawe@gmail.com>
2023-08-23 02:03:54 +0530

    Saved work

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { join } = require('path');
+
+const traverse = (dir) => {
+    const subfolders = fs.statSync(dir).isDirectory() && fs.readdirSync(dir);
+
+    if (subfolders) {
+        subfolders.forEach((folderName) => {
+            const fullPath = join(dir, folderName);
+
+            if (folderName === 'node_modules') {
+                fs.rmSync(join(dir, 'node_modules'), { recursive: true, force: true });
+                return;
+            }
+
+            traverse(fullPath);
+        });
+    }
+};
+
+traverse(process.cwd());
