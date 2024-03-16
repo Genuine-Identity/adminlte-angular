c8f4ed629d9109d0a2ced8b4bac7cca59a0c10b3
Genuine-Identity <girish.nandgawe@gmail.com>
2024-03-17 00:28:05 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NgxDatatableModule } from "@swimlane/ngx-datatable";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import "jquery";
-import "bootstrap";
-import "chart.js";
+import 'jquery';
+import 'bootstrap';
+import 'chart.js';
 // import "@ng-bootstrap/ng-bootstrap";
 
-import "ionicons";
-import "lodash";
-import "rxjs";
+import 'ionicons';
+import 'lodash';
+import 'rxjs';
 // import "../../src/assets/javascript/adminlte.ts";
 // import "../../src/assets/javascript/demo.ts";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject } from 'rxjs';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
-import { CoreModule } from "./core/core.module";
-import { AppRoutingModule } from "./app.routing.module";
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app.routing.module';
 
-import { NgSelectModule } from "@ng-select/ng-select";
+import { NgSelectModule } from '@ng-select/ng-select';
+import 'zone.js';
 
 @NgModule({
   imports: [
@@ -32,7 +33,7 @@ import { NgSelectModule } from "@ng-select/ng-select";
     NgxDatatableModule,
     ReactiveFormsModule,
     CoreModule,
-    NgSelectModule,    
+    NgSelectModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
